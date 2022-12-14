54be94affa787713043a78953f3e91adcb8a19f9
Jorge Quintui <jorgealequinn@gmail.com>
2022-12-14 01:16:03 -0300

    ADD: Settings config

---
+++ settings.py
@@ -0,0 +1,7 @@
+from decouple import config
+
+## Your settings
+HOST=config('EMAIL_HOST_SERVER')
+PORT=config('EMAIL_PORT_SERVER')
+EMAIL=config('EMAIL_HOST_USER')
+PASSWORD=config('EMAIL_HOST_PASSWORD')
